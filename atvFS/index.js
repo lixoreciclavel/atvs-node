49595626c7ca357c6ab5c133ad8bb68626893c7d
Levy Cordeiro <108874182+lixoreciclavel@users.noreply.github.com>
2023-07-07 11:51:47 -0300

    Add files via upload

---
+++ atvFS/index.js
@@ -0,0 +1,58 @@
+const fs = require("fs");
+// atv01
+
+// fs.readFile('texto01.txt', 'utf8', (err,data)=>{
+//     if(err) {
+//         console.log(err)
+//         return
+//     }
+//     console.log(data)
+// })
+
+// atv02
+// fs.writeFile('texto02.txt','tres',(err)=>{
+//     if(err){
+//         console.error(err)
+//         return
+//     }
+//     console.log('Deu bom!')
+// })
+
+//atv03
+// fs.writeFile('texto02.txt', 'sera?', (err)=>{
+//     if(err){
+//         console.error(err)
+//     }
+//     console.log('teste')
+// })
+
+// atv04
+// fs.copyFile('texto01.txt', 'texto03.txt', (err)=>{
+//     if(err){
+//         console.error(err)
+//     }
+//     console.log('teste')
+// })
+
+// atv05
+// fs.rename('texto03.txt', 'renomeado.txt', (err)=>{
+//     if(err){
+//         console.error(err)
+//     }
+//     console.log('teste')
+// })
+
+// atv06
+// fs.unlink('texto01.txt', (err)=>{
+//     if(err){
+//         console.error(err)
+//     }
+//     console.log('teste')
+// })
+
+// atv07
+// if (fs.existsSync("texto02.txt")) {
+//   console.log("O arquivo existe");
+// } else {
+//   console.log("O arquivo não existe");
+// }
---
+++ atvPath/index.js
@@ -0,0 +1,25 @@
+const path = require('path')
+
+// atv01
+// const customPath = '/usr/local/bin/script.js'
+// console.log(path.basename(customPath))
+
+// atv02
+// const absolutePath = path.resolve('src', 'model')
+// const relativePath = path.relative('src', 'model')
+
+// console.log(absolutePath, + "é absolute")
+// console.log(relativePath, + "é relative")
+
+// atv03
+// const midFolder = 'relatios'
+// const fileName = 'textt.txt'
+
+// const finalPath = path.join('/', 'arquivos', midFolder, fileName)
+// console.log(finalPath)
+
+// atv04
+// const normalizePath = path.normalize("/caminho/absoluto/../outraPasta/./arquivo.txt")
+// console.log(normalizePath)
+
+// atv05
